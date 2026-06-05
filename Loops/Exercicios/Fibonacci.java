1183b9d9a2c66abb61ad62574e91d0898dab0ac7
Valcyr Junior <vieiravalcyr@gmail.com>
2026-06-05 06:26:13 -0300

    Some fixes in Fibonacci logic

--- Loops/Exercicios/Fibonacci.java
+++ Loops/Exercicios/Fibonacci.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
-public class Fibonacci {
+public class Fibonacci{
     public static void main (String[] args){
         Scanner scan = new Scanner(System.in);
-
         System.out.println("Do you want the Fibonacci sequence up to which number?");
-        int limit = 500;
+        int limit = scan.nextInt();
         int actual = 1;
         int next = 0;
         int previous = 0;
         System.out.println(previous);
-        while (previous > limit){
+        while (previous < limit){
             System.out.println(previous);
             next = previous + actual;
             previous = actual;
