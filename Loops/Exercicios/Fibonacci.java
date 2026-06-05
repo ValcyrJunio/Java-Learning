b29750fd27c29656277676e8e4c2052139f0ce69
Valcyr Junior <vieiravalcyr@gmail.com>
2026-06-05 08:49:32 -0300

    Update Fibonacci.java

--- Loops/Exercicios/Fibonacci.java
+++ Loops/Exercicios/Fibonacci.java
@@ -1,14 +1,11 @@
-import java.util.Scanner;
-public class Fibonacci{
+package com.aula17.loopFor.Exercicios;
+public class Fibonacci {
     public static void main (String[] args){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Do you want the Fibonacci sequence up to which number?");
-        int limit = scan.nextInt();
+        int limit = 500;
         int actual = 1;
         int next = 0;
         int previous = 0;
-        System.out.println(previous);
-        while (previous < limit){
+        while (previous <= limit){
             System.out.println(previous);
             next = previous + actual;
             previous = actual;
